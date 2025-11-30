59fbe6c41bfa36917719fa227a2f385746ed0450
Praveen Singh <praveensingh.199@gmail.com>
2025-11-30 20:53:37 +0530

    Around Advice

---
+++ src/main/java/com/spring/rest/aspect/PerformanceMonitorAspect.java
@@ -0,0 +1,25 @@
+package com.spring.rest.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class PerformanceMonitorAspect {
+
+  public static final Logger LOGGER = LoggerFactory.getLogger(PerformanceMonitorAspect.class);
+
+  @Around("execution (* com.spring.rest.service.JobService.*(..))")
+  public Object monitorTime(ProceedingJoinPoint jp) throws Throwable {
+
+    long start = System.currentTimeMillis();
+    Object obj = jp.proceed();
+    long end = System.currentTimeMillis();
+    LOGGER.info("Time taken by: " + jp.getSignature().getName() + " " + (end - start) + " ms");
+    return obj;
+  }
+}
